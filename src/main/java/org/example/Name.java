5d4039d20bca3b69da99b09ad218591d6757c10b
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-01-20 13:14:54 +0000

    Implemented tests

--- src/main/java/org/example/Name.java
+++ src/main/java/org/example/Name.java
@@ -1,11 +1,21 @@
 package org.example;
 
 public class Name {
-    public Name() {
+    private String name;
 
+    private int numericValue;
+
+    public String getName() {
+        return name;
     }
-
-    public int numericValue(String input) {
-        return 0;
+    public int getNumericValue() {
+        return numericValue;
+    }
+    private Name(String input) {
+        this.name = input;
+        this.numericValue = 0;
+    }
+    public static Name makeName(String input) {
+        return new Name(input);
     }
 }
