44eeb655a7da1e9e8ce06f1f9c516fe6ad1946e3
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-01-20 12:29:53 +0000

    First implementation of numericValue method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public int numericValue(String input) {
+        return 0;
+    }
 }
